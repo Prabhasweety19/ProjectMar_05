cff570928bec751d25a8f88fec3737689f508bb1
Prabhasweety19 <prabhaprathima19@gmail.com>
2022-12-21 19:45:56 +0530

    prathi's code

--- src/main/java/org/ProjectMar/ProjectMar_05.java
+++ src/main/java/org/ProjectMar/ProjectMar_05.java
@@ -4,7 +4,12 @@ public class ProjectMar_05 {
 
 	public static void main(String[] args) {
 	System.out.println("test1");
-
+    System.out.println("test4");
+    System.out.println("test5");
+	
+	
+	
+	
 	}
 
 }
